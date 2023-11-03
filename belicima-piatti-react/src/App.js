175e53fff80f5776196a8addeceaaab88e028c4e
korniykom <korniykom@gmail.com>
2023-11-03 16:57:45 +0200

    reorginize files in react project

--- belicima-piatti-react/src/App.js
+++ belicima-piatti-react/src/App.js
@@ -1,19 +1,19 @@
-import Card from "./Card";
+import Card from "./components/Card/Card";
 import Nav from "./components/Nav/Nav";
 import ShoppingList from "./components/Shopping-List/Shopping-List";
 import StepList from "./components/Step-List/Step-List";
-import dishes from "./Dishes.json";
-import ingredients from "./DishesDetails.json";
-import steps from "./DishesDetails.json";
+import dishes from "./data/Dishes.json";
+import ingredients from "./data/DishesDetails.json";
+import steps from "./data/DishesDetails.json";
 const id = 1;
 export default function App() {
   return (
     <div>
       <Nav />
-      <StepList StepList={steps.DishesDetails.at(id).steps} />
+      {/* <StepList StepList={steps.DishesDetails.at(id).steps} />
       <ShoppingList
         ShoppingList={ingredients.DishesDetails.at(id).ingredients}
-      />
+      /> */}
 
       {dishes.dishes.map((dish) => (
         <Card
