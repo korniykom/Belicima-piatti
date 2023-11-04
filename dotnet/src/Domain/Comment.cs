61790d6bcbac427ea5748b2cd364e06ec2140e3f
korniykom <korniykom@gmail.com>
2023-11-04 22:20:19 +0200

    removed id property from Comment class (there was no point in it)

--- dotnet/src/Domain/Comment.cs
+++ dotnet/src/Domain/Comment.cs
@@ -1,12 +1,10 @@
 namespace src.Domain {
     public class Comment {
         //Constructor
-        public Comment(int id, string commentText, string commentatorName) {
-            this.id = id;
+        public Comment(string commentText, string commentatorName) {
             this.commentText = commentText;
             this.commentatorName = commentatorName;
         }
-        public int id { get; set; }
         public string commentText { get; set;}
         public string commentatorName { get; set;}
     }
