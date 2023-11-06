4f96ebbec603e0e59899065d2ed9a888d33db091
AndriiReshetniuk <andriyreshet@gmail.com>
2023-11-06 16:32:32 +0200

    Add error loading component

---
+++ belicima-piatti-react/src/components/ErrorLoading/Error.js
@@ -0,0 +1,17 @@
+import "./Error.css";
+export default function App() {
+  return (
+    <div className="div1">
+      <div className="sadMen">
+        <img
+          src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR6v5YaszroqdxzdabfVqEb0QvgyaKiGnyGoRuzk62jwCottiDX"
+          width="75px"
+          height="55px;"
+          className="sadImg"
+        />
+      </div>
+      <h1 className="inside">Помилка:</h1>
+      <h3 className="inside">Не вдалося завантажити сторінку</h3>
+    </div>
+  );
+}
