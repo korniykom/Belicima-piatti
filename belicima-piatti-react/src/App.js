7b4d183f84f73b6a522764a6816a91982245143f
Bogdan <malyarchuk.bogdan@lll.kpi.ua>
2023-11-07 15:37:13 +0200

    created Loading component

--- belicima-piatti-react/src/App.js
+++ belicima-piatti-react/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Menupage from "./pages/Homepage";
 import Infopage from "./pages/Infopage";
 import Accountpage from "./pages/Accountpage";
+import Loadingpage from "./pages/Loadingpage";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="menu" element={<Menupage />} />
         <Route path="info" element={<Infopage />} />
         <Route path="account" element={<Accountpage />} />
+        <Route path="loading" element={<Loadingpage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ belicima-piatti-react/src/pages/Loadingpage.js
@@ -0,0 +1,21 @@
+import "../components/Loading/Loading.css";
+
+function Loadingpage() {
+  return (
+    <html>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+        rel="stylesheet"
+      ></link>
+      <div class="loading-container">
+        <h2 class="loading-title">Loading State</h2>
+        <div class="loading-bar">
+          <p class="white-back">Loading...</p>
+          <div class="spinner"></div>
+        </div>
+      </div>
+    </html>
+  );
+}
+
+export default Loadingpage;
