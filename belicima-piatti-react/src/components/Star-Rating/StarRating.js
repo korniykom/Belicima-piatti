592fd93dc310dff427eeb7164339f440d85ec310
korniykom <korniykom@gmail.com>
2023-11-09 15:46:30 +0200

    created comment component

--- belicima-piatti-react/src/components/Star-Rating/StarRating.js
+++ belicima-piatti-react/src/components/Star-Rating/StarRating.js
@@ -1,15 +1,12 @@
 import "./StarRating.css";
-  import {FaStar} from "react-icons/fa";
-  
+import { FaStar } from "react-icons/fa";
 
 export default function StarRating({ rating }) {
   return (
     <div className="containerStyle">
       <div className="starContainerStyle">
         {Array.from({ length: 5 }, (_, i) => (
-          <span key={i}>
-            {i < rating ? <YellowStar /> : <WhiteStar />}
-          </span>
+          <span key={i}>{i < rating ? <YellowStar /> : <WhiteStar />}</span>
         ))}
         <span className="textStyle ">{rating}</span>
       </div>
@@ -20,14 +17,14 @@ export default function StarRating({ rating }) {
 function WhiteStar() {
   return (
     <div className="star">
-       <FaStar size={35} color={"white"}  />
+      <FaStar size={35} color={"#aaa"} />
     </div>
   );
 }
 function YellowStar() {
   return (
-    <div className="star">
-       <FaStar size={35} color={"yellow"}  />
-    </div>
+    <span className="yellowStar">
+      <FaStar size={35} color={"orange"} />
+    </span>
   );
 }
--- belicima-piatti-react/src/pages/Accountpage.js
+++ belicima-piatti-react/src/pages/Accountpage.js
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
-import StarRating from "../components/Star-Rating/StarRating"
+import Comment from "../components/Comment/Comment";
 
-function Accountpage(){
-    return(
-        <div className="main-block">
-            <PageNav />
-            <StarRating rating={3}/>
-            <h1>Account</h1>
-            <Link to="account">Account</Link>
-        </div>
-    );
+function Accountpage() {
+  return (
+    <div className="main-block">
+      <PageNav />
+      <Comment />
+      <h1>Account</h1>
+      <Link to="account">Account</Link>
+    </div>
+  );
 }
 
 export default Accountpage;
