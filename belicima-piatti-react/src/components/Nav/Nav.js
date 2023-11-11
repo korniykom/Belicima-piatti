6ded84dd1aee8683d238713e40ad8a2744def3b9
Bogdan <malyarchuk.bogdan@lll.kpi.ua>
2023-11-11 20:36:33 +0200

    fixed path to page of detail view & fixed nav component & created base view

--- belicima-piatti-react/src/components/Nav/Nav.js
+++ belicima-piatti-react/src/components/Nav/Nav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom"; // Імпорт компонента Link з React Router
+import { Link } from "react-router-dom";
 import "./Nav.css";
 
 export default function Nav({ backLink, title }) {
