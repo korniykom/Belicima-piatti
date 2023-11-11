45126266989f03a27239ff76c1bce41983798eae
korniykom <korniykom@gmail.com>
2023-11-11 03:05:57 +0200

    created detailed dish page

--- belicima-piatti-react/src/App.js
+++ belicima-piatti-react/src/App.js
@@ -5,11 +5,13 @@ import Menupage from "./pages/Homepage";
 import Infopage from "./pages/Infopage";
 import Accountpage from "./pages/Accountpage";
 import Loading from "./components/Loading/Loading";
+import DetailedDish from "./pages/DetailedDish";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes className="main-block">
+        <Route path="dish/:category" element={<DetailedDish />} />
         <Route path="menu" element={<Menupage />} />
         <Route path="info" element={<Infopage />} />
         <Route path="account" element={<Accountpage />} />
