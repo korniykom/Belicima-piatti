20dc3bd6fe3216a94dc467d61e78a8570cc7076c
korniykom <korniykom@gmail.com>
2023-11-13 19:04:12 +0200

    refactoring comment-rest code

--- dotnet/src/Repositories/CommentsRepository.cs
+++ dotnet/src/Repositories/CommentsRepository.cs
@@ -3,30 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using src.Domain;
 
-namespace src.Repositories {
-    public class CommentRepository {
+namespace src.Repositories 
+{
+    public class CommentRepository 
+    {
         Dictionary<int, List<Comment>> commentsDictionary = new ();
         private int currentId = 1;
-        //Constructor
-        public CommentRepository() {
-            Comment testComment = new (0, "test text", "test name");
-
-            commentsDictionary.Add(0, new List<Comment>());
-            commentsDictionary[0].Add(testComment);
+        public CommentRepository() 
+        {
+            comments.Add(new Comment(
+                "Delicious and satisfying, this dish is a culinary masterpiece that tickles your taste buds with every bite. A true symphony of flavors that leaves you craving for more. Bon appétit!",
+                "User#0"
+            ));
         }
 
-        public List<Comment> GetAllComments() {
+        public List<Comment> GetAllComments() 
+        {
             List<Comment> allComments = new ();
-            foreach (var (key, value) in commentsDictionary) {
+            foreach (var (key, value) in commentsDictionary) 
+            {
                 allComments.AddRange(value);
             }
             return allComments;
         }
-        public List<Comment> GetCommentByRecipeId(int id) {
+        public List<Comment> GetCommentByRecipeId(int id) 
+        {
             return commentsDictionary[id];
         }
-        public Comment AddComment(Comment comment) {
-            if (!commentsDictionary.ContainsKey(comment.recipeId)) {
+        public Comment AddComment(Comment comment) 
+        {
+            if (!commentsDictionary.ContainsKey(comment.recipeId)) 
+            {
               commentsDictionary.Add(comment.recipeId, new List<Comment>());
             }
             commentsDictionary[comment.recipeId].Add(comment);
