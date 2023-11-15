da647af5964cc7e597fc7b90ec7a3886650364a7
AndriiReshetniuk <andriyreshet@gmail.com>
2023-11-15 16:22:21 +0200

    Resizing a componet

--- belicima-piatti-react/src/components/ErrorLoading/Error.js
+++ belicima-piatti-react/src/components/ErrorLoading/Error.js
@@ -3,11 +3,11 @@ import {BiSolidError} from 'react-icons/bi';
 export default function ErrorLoading() {
   return (
     <div className="ErrorLoadingBody">
-      <div className="sadMen">
-        <BiSolidError size = "55px"  className="sadImg"/>
+      <h1 className="InsideOfErrorLoading1">Помилка:</h1>
+      <h3 className="InsideOfErrorLoading2">Не вдалося завантажити сторінку</h3>
+      <div className="ErrorImgBlock">
+        <BiSolidError size = "55px"  className="ErrorLoadingImg"/>
       </div>
-      <h1 className="InsideOfErrorLoading">Помилка:</h1>
-      <h3 className="InsideOfErrorLoading">Не вдалося завантажити сторінку</h3>
     </div>
   );
 }
