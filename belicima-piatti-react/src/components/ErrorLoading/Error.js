8483c239988048bef2376b6225d9c9e1077584a2
Bogdan <malyarchuk.bogdan@lll.kpi.ua>
2023-11-16 17:27:49 +0200

    some little fixes

--- belicima-piatti-react/src/components/ErrorLoading/Error.js
+++ belicima-piatti-react/src/components/ErrorLoading/Error.js
@@ -1,12 +1,13 @@
+import React from "react";
 import "./Error.css";
-import {BiSolidError} from 'react-icons/bi';
+import { BiSolidError } from "react-icons/bi";
 export default function ErrorLoading() {
   return (
     <div className="ErrorLoadingBody">
       <h1 className="InsideOfErrorLoading1">Помилка:</h1>
       <h3 className="InsideOfErrorLoading2">Не вдалося завантажити сторінку</h3>
       <div className="ErrorImgBlock">
-        <BiSolidError size = "55px"  className="ErrorLoadingImg"/>
+        <BiSolidError size="55px" className="ErrorLoadingImg" />
       </div>
     </div>
   );
