866ef0a3a5b46394c9b717f5d690df7ce4e69688
Bogdan <malyarchuk.bogdan@lll.kpi.ua>
2023-11-18 13:45:51 +0200

    changed routes

--- belicima-piatti-react/src/pages/Dish.js
+++ belicima-piatti-react/src/pages/Dish.js
@@ -1,12 +1,19 @@
 import { useParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import Nav from "../components/Nav/Nav";
+
 export default function Dish() {
   const { id } = useParams();
+  const [searchParams] = useSearchParams();
+  var page = searchParams.get("page");
+  var pageSize = searchParams.get("pageSize");
 
   return (
     <div>
       <Nav backLink="/" title={`Страва ${id}`} />
       <div>{id}</div>
+      <div>{page}</div>
+      <div>{pageSize}</div>
     </div>
   );
 }
