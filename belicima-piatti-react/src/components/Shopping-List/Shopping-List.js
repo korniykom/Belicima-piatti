ccd12155f9ba1b3335ee49851712ae93c928ec72
korniykom <korniykom@gmail.com>
2023-11-23 22:36:19 +0200

    added button Дізнатися більше and functionality for it

--- belicima-piatti-react/src/components/Shopping-List/Shopping-List.js
+++ belicima-piatti-react/src/components/Shopping-List/Shopping-List.js
@@ -1,18 +1,47 @@
 import ShoppingListItem from "./Shopping-List-Item";
 import "./Shopping-List.css";
+import { useState } from "react";
 export default function ShoppingList({ ShoppingList }) {
+  const [maxDisplay, setMaxDisplay] = useState(4);
+  const [isOpen, setIsOpen] = useState(false);
+  function handleOpen() {
+    setMaxDisplay(20);
+    setIsOpen(!isOpen);
+  }
+  function handleClose() {
+    setMaxDisplay(4);
+    setIsOpen(!isOpen);
+  }
   return (
     <>
       <h2>Shopping List</h2>
-      {ShoppingList.map((item) => (
-        <ShoppingListItem
-          product={item.name}
-          price={item.price}
-          calories={item.calories}
-          key={item.id}
-        />
+
+      {ShoppingList.map((item, index) => (
+        <>
+          {index < maxDisplay && (
+            <ShoppingListItem
+              product={item.name}
+              price={item.price}
+              calories={item.calories}
+              key={item.id}
+            />
+          )}
+        </>
       ))}
-      {/* {console.log(typeof ShoppingList)} */}
+
+      {ShoppingList.length > 4 && (
+        <div className="box">
+          {!isOpen ? (
+            <div onClick={handleOpen} className="more-button">
+              Дізнатися більше
+            </div>
+          ) : (
+            <div onClick={handleClose} className="more-button">
+              Приховати
+            </div>
+          )}
+        </div>
+      )}
     </>
   );
 }
