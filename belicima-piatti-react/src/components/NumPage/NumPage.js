36e6ff759c9da695e77afd32f9bf155ef5ffe25c
korniykom <korniykom@gmail.com>
2023-11-27 15:41:29 +0200

    NumPage component now working

--- belicima-piatti-react/src/components/NumPage/NumPage.js
+++ belicima-piatti-react/src/components/NumPage/NumPage.js
@@ -1,28 +1,26 @@
 import { Link } from "react-router-dom";
 import React, { useState } from "react";
 
-const PageNavigator = () => {
+export default function PageNavigator({ category }) {
   const [currentPage, setCurrentPage] = useState(1);
 
-  const goToNextPage = () => {
+  function handleNextPage() {
     setCurrentPage(currentPage + 1);
-  };
+  }
 
-  const goToPrevPage = () => {
+  const handlePrevPage = () => {
     setCurrentPage(currentPage - 1);
   };
 
   return (
     <div>
-      <Link to={`/dishes?pageSize=10&page=${currentPage}`}>
-        <button onClick={goToPrevPage} disabled={currentPage === 1}>
+      <Link to={`/dishes/${category}?pageSize=10&page=${currentPage}`}>
+        <button onClick={handlePrevPage} disabled={currentPage === 1}>
           {"<"}
         </button>
         <span> {currentPage} </span>
-        <button onClick={goToNextPage}>{">"}</button>
+        <button onClick={handleNextPage}>{">"}</button>
       </Link>
     </div>
   );
-};
-
-export default PageNavigator;
+}
