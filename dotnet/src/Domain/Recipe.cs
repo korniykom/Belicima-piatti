7862148a6aba0b198239fdd9c8c769cac3bdddb0
tr23malyarchuk <123400836+tr23malyarchuk@users.noreply.github.com>
2023-11-29 15:21:56 +0200

    added new information about ingredients

--- dotnet/src/Domain/Recipe.cs
+++ dotnet/src/Domain/Recipe.cs
@@ -34,5 +34,8 @@ namespace src.Domain {
         public string name { get; set; }
         public double price { get; set; }
         public int calories { get; set; }
+        public double protein { get; set; }
+        public double carbs { get; set; }
+        public double fat { get; set; }
     }
 }
