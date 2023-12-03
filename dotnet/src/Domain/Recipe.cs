5f2dfb0282f4d60e4242383a8b07530f30e6723b
tr23malyarchuk <malyarchuk.bogdan@lll.kpi.ua>
2023-12-03 20:30:15 +0200

    .

--- dotnet/src/Domain/Recipe.cs
+++ dotnet/src/Domain/Recipe.cs
@@ -4,11 +4,7 @@ namespace src.Domain {
         public string name { get; set; }
         public string img {get; set;}
         public string description { get; set; }
-<<<<<<< HEAD
         public string category { get; set; }
-=======
-        public string category {get; set;}
->>>>>>> origin/main
         public double score { get; set; }
         public int duration { get; set; }
         public double price { get; set; }
