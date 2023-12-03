09ce3558f8bb7062fa62937a202b5f92e9708dad
tr23malyarchuk <malyarchuk.bogdan@lll.kpi.ua>
2023-12-03 20:06:56 +0200

    finalization of CategoryComponent

--- belicima-piatti-react/src/components/Category/Category.js
+++ belicima-piatti-react/src/components/Category/Category.js
@@ -12,6 +12,7 @@ export default function Category({ category }) {
           <div className="text">{category}</div>
         </div>
       </div>
+
       <Link to="category"></Link>
     </div>
   );
