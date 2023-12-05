0c01829f9eaf655d84067d91602986a20030cbcf
korniykom <korniykom@gmail.com>
2023-12-05 14:21:16 +0200

    fixed rerendering page, bug with displaying page number remained

--- belicima-piatti-react/src/components/NumPage/NumPage.js
+++ belicima-piatti-react/src/components/NumPage/NumPage.js
@@ -1,35 +1,45 @@
 import { Link } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import "./NumPage.css";
 import { useNavigate } from "react-router-dom";
 
-export default function PageNavigator({ category, page, pageSize = 5 }) {
+export default function PageNavigator({
+  category,
+  page,
+  pageSize = 3,
+  ignored,
+  forceUpdate,
+}) {
   const [currentPage, setCurrentPage] = useState(+page);
   const navigate = useNavigate();
 
   function handleNextPage() {
     setCurrentPage(currentPage + 1);
     navigate(`/dishes/${category}?pageSize=${pageSize}&page=${currentPage}`);
+    forceUpdate();
   }
 
   const handlePrevPage = () => {
     setCurrentPage(currentPage - 1);
     navigate(`/dishes/${category}?pageSize=${pageSize}&page=${currentPage}`);
+    forceUpdate();
   };
 
   return (
     <div>
-      <button
-        class="buttonNumPage"
-        onClick={handlePrevPage}
-        disabled={currentPage <= 1}
-      >
-        {"<"}
-      </button>
-      <span class="numPage"> {page} </span>
-      <button class="buttonNumPage" onClick={handleNextPage}>
-        {">"}
-      </button>
+      <div class="navNumPage">
+        <button
+          class="buttonNumPage"
+          onClick={handlePrevPage}
+          disabled={currentPage < 1}
+        >
+          {"<"}
+        </button>
+        <span class="numPage"> {page} </span>
+        <button class="buttonNumPage" onClick={handleNextPage}>
+          {">"}
+        </button>
+      </div>
     </div>
   );
 }
