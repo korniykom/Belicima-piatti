ca889ce7f09b933ac7379b9522b91b9310734818
SkhtskIryna <foxira242@gmail.com>
2023-12-12 13:07:55 +0200

    text

--- belicima-piatti-react/src/components/SideNav/SideNav.js
+++ belicima-piatti-react/src/components/SideNav/SideNav.js
@@ -1,15 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./SideNav.css";
-import {FaBars} from 'react-icons/fa';
-const SideNav = () =>
-{
-   const [Nav, setNav] = useState(false)
-   return <div class = "SideNavContainer"><FaBars size = "5vh" className='SideNavIcon' onClick={()=>setNav(!Nav)}/>
-   <div className = {Nav ? "Active" : "SideNavMenu"}>
-     <div className = "insideSideNav">
-      <h2 className='SideNavMenuItem'>Menu</h2>
+import { FaBars } from "react-icons/fa";
+
+const SideNav = () => {
+  const [Nav, setNav] = useState(false);
+  return (
+    <div class="SideNavContainer">
+      <FaBars size="5vh" className="SideNavIcon" onClick={() => setNav(!Nav)} />
+      <div className={Nav ? "Active" : "SideNavMenu"}>
+        <div className="insideSideNav">
+          <h2 className="SideNavMenuItem">Menu</h2>
+          <Link class="menu-link" to="">
+            Recipe of the day
+          </Link>
+          <Link
+            class="menu-link"
+            to="http://localhost:3000/dishes/my_recipes?page=1&pageSize=3"
+          >
+            My recipes
+          </Link>
+          <Link
+            class="menu-link"
+            to="http://localhost:3000/dishes/top?page=1&pageSize=3"
+          >
+            Top recipes
+          </Link>
+        </div>
       </div>
-   </div>
-   </div>;
-}
+    </div>
+  );
+};
 export default SideNav;
