7e3c03dcbb371ac652914b7e5efb5eb8976868f8
korniykom <korniykom@gmail.com>
2023-12-18 22:31:12 +0430

    fixed link in SideNav.js

--- belicima-piatti-react/src/components/SideNav/SideNav.js
+++ belicima-piatti-react/src/components/SideNav/SideNav.js
@@ -11,18 +11,18 @@ const SideNav = () => {
       <div className={Nav ? "Active" : "SideNavMenu"}>
         <div className="insideSideNav">
           <h2 className="SideNavMenuItem">Menu</h2>
-          <Link class="menu-link" to="">
+          <Link class="menu-link" to="http://localhost:3000/dishdetail/7">
             Recipe of the day
           </Link>
           <Link
             class="menu-link"
-            to="http://localhost:3000/dishes/my_recipes?page=1&pageSize=3"
+            to="http://localhost:3000/dishes?pageSize=10&country=&category=%D0%9C%D0%BE%D1%97%20%D1%80%D0%B5%D1%86%D0%B5%D0%BF%D1%82%D0%B8&page=1"
           >
             My recipes
           </Link>
           <Link
             class="menu-link"
-            to="http://localhost:3000/dishes/top?page=1&pageSize=3"
+            to="http://localhost:3000/dishes?pageSize=10&country=&category=Топ&page=1"
           >
             Top recipes
           </Link>
